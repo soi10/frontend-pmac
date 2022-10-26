598f95ff49aa527a61d51ae2a3543e7d63f084f7
nongsoi10 <nongsoi10007@gmail.com>
2022-10-26 20:59:33 +0700

    fix pages login

--- views/index.php
+++ views/index.php
@@ -18,7 +18,7 @@ include "head.php"
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6 bg-login-image"></div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
@@ -33,8 +33,7 @@ include "head.php"
                                         </div>
                                         <div class="form-group">
                                             <input type="password" class="form-control form-control-user" id="password"
-                                                placeholder="รหัสผ่านเข้าเครื่องคอมพิวเตอร์" name="password"
-                                                value="">
+                                                placeholder="รหัสผ่านเข้าเครื่องคอมพิวเตอร์" name="password" value="">
                                         </div>
                                         <button type="submit" class="btn btn-primary btn-user btn-block">
                                             Login

bba51e83f7872676ff3bac6e769665858efb2178
Yongyuth Chaipa <nas.coke@gmail.com>
2024-11-06 20:26:12 +0700

    add service login keycloak

--- views/index.php
+++ views/index.php
@@ -1,4 +1,6 @@
 <!DOCTYPE html>
+<link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.4/components/logins/login-4/assets/css/login-4.css">
 <html lang="en">
 
 <?php
@@ -10,77 +12,42 @@ include "head.php"
     <div class="container">
 
         <!-- Outer Row -->
-        <div class="row justify-content-center">
-
-            <div class="col-xl-10 col-lg-12 col-md-9">
-
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-6 bg-login-image"></div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Power Meter And Current Transformer Tester
-                                        </h1>
+        <!-- Login 4 - Bootstrap Brain Component -->
+        <section class="p-3 p-md-4 p-xl-5">
+            <div class="container">
+                <div class="card border-light-subtle shadow-sm">
+                    <div class="row g-0">
+                        <div class="col-12 col-md-6">
+                            <img class="img-fluid rounded-start w-100 h-100 object-fit-cover" loading="lazy"
+                                src="../img/pic3.png" alt="BootstrapBrain Logo">
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <div class="card-body p-3 p-md-4 p-xl-5">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="mb-5">
+                                            <h3>Power Meter And Current Transformer Tester</h3>
+                                        </div>
                                     </div>
-                                    <form class="user" id="myform1">
-                                        <div class="form-group">
-                                            <input type="text" class="form-control form-control-user" id="username"
-                                                aria-describedby="emailHelp" placeholder="รหัสพนักงาน" name="username"
-                                                value="">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" id="password"
-                                                placeholder="รหัสผ่านเข้าเครื่องคอมพิวเตอร์" name="password" value="">
-                                        </div>
-                                        <button type="submit" class="btn btn-primary btn-user btn-block">
-                                            Login
-                                        </button>
-                                        <div class="mt-3" id="loading">
-                                            <div class="spinner-border text-primary" role="status">
-                                                <span class="sr-only">Loading...</span>
-                                            </div>
-                                            <div class="spinner-border text-secondary" role="status">
-                                                <span class="sr-only">Loading...</span>
-                                            </div>
-                                            <div class="spinner-border text-success" role="status">
-                                                <span class="sr-only">Loading...</span>
-                                            </div>
-                                            <div class="spinner-border text-danger" role="status">
-                                                <span class="sr-only">Loading...</span>
-                                            </div>
-                                            <div class="spinner-border text-warning" role="status">
-                                                <span class="sr-only">Loading...</span>
-                                            </div>
-                                            <div class="spinner-border text-info" role="status">
-                                                <span class="sr-only">Loading...</span>
-                                            </div>
-                                            <div class="spinner-border text-light" role="status">
-                                                <span class="sr-only">Loading...</span>
-                                            </div>
-                                            <div class="spinner-border text-dark" role="status">
-                                                <span class="sr-only">Loading...</span>
-                                            </div>
-                                        </div>
-                                    </form>
-                                    <br>
-                                    <br>
-                                    <br>
-                                    <br>
-                                    <br>
-                                    <br>
-                                    <br>
                                 </div>
+                                <form action="#!">
+                                    <div class="row gy-3 gy-md-4 overflow-hidden">
+                                        <div class="col-12">
+                                            <div class="d-grid">
+                                                <a id="loginButton" class="btn bsb-btn-xl btn-primary m-5"
+                                                    type="submit"><i class="fa-solid fa-arrow-right-to-bracket me-2"
+                                                        style="font-size: 25px; vertical-align: middle"></i>
+                                                    <span style="vertical-align: middle">Login</span></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
-
-        </div>
+        </section>
 
     </div>
 
@@ -97,39 +64,188 @@ $(document).ready(function() {
     $("#loading").hide();
 });
 
-$("#myform1").on("submit", function(e) {
-    e.preventDefault(); // ปิดการใช้งาน submit ปกติ เพื่อใช้งานผ่าน ajax
-    $("#loading").show();
-    // เตรียมข้อมูล form สำหรับส่งด้วย  FormData Object
+// $("#loadingSpinner").show();
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
 
-    if ((!username) || (!password)) {
-        alert("กรอกข้อมูลไม่ครบ !!")
-    } else {
-        $.ajax({
-            type: "post",
-            url: "https://eitne2.com/nongha/api/login/",
-            contentType: "application/json",
-            dataType: "json",
-            data: JSON.stringify({
-                username: $("#username").val(),
-                password: $("#password").val(),
-            }),
-            success: function(response) {
-                if (response.statusCode == 200) {
-                    localStorage.setItem('Peacode', response.user.Peacode);
-                    localStorage.setItem('Username', response.user.Username);
-                    localStorage.setItem('FirstName', response.user.FirstName);
-                    localStorage.setItem('LastName', response.user.LastName);
-                    localStorage.setItem('BaName', response.user.BaName);
-                    window.location = "../views/dashboard.php";
-                } else {
-                    alert("เข้าระบบมีปัญหาไม่สามารถเข้าระบบได้")
-                }
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function setSessionStorage(key, value) {
+    sessionStorage.setItem(key, value);
+}
+
+function getSessionStorage(key) {
+    return sessionStorage.getItem(key);
+}
+
+async function fetchToken(code) {
+    const url = `${location.protocol}//${location.host}`;
+    const response = await fetch(
+        url + "/pmac/serviceslogin/proxy2.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
             },
-            error: function(response) {
-                console.log(response);
+            body: new URLSearchParams({
+                code: code,
+                client_id: "pea-queuex-softkey",
+                client_secret: "ZnO5xEia9YEJdrznI0YeU399ClJWYtVl",
+                grant_type: "authorization_code",
+                redirect_uri: url + "/pmac/views/index.php",
+            }),
+        }
+    );
+
+    if (response.ok) {
+        const data = await response.json();
+        return data.access_token;
+    } else {
+        console.error("Error fetching token:", response.statusText);
+        return null;
+    }
+}
+
+async function fetchUserInfo(token) {
+    const url = `${location.protocol}//${location.host}`;
+    const response = await fetch(
+        `${url}/pmac/serviceslogin/proxy3.php?token=${token}`
+    );
+
+    if (response.ok) {
+        const data = await response.json();
+
+        // displayValues(data);
+        if (data.hr_employee_id) {
+            console.log('proxy3:', data);
+            setSessionStorage('hr_employee_id', data.hr_employee_id); // Store employee ID in session storage
+            setSessionStorage('hr_data', JSON.stringify(data)); // Store employee ID in session storage
+        }
+    } else {
+        console.error("Error fetching user info:", response.statusText);
+    }
+}
+
+function redirectToIndex(data) {
+    console.log(data.hr_fullname_th, " ", data.hr_employee_id);
+    window.location.href = "./dashboard.php";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const params = new URLSearchParams(window.location.search);
+    let code = params.get("code");
+    console.log('code:', code);
+
+    if (code) {
+        // Only fetch token and user info if the code is present
+        console.log('in fetchToken');
+
+        fetchToken(code).then(async (token) => { // Change to an async function
+            if (token) {
+                setCookie('access_token', token, 1); // Store token in a cookie for 1 day
+                console.log('Cookie stored.');
+
+                await fetchUserInfo(token); // Await the completion of fetchUserInfo
+
+                const hr_data = JSON.parse(getSessionStorage('hr_data'));
+                console.log('hr_data_fetch:', hr_data);
+                localStorage.setItem('Username', hr_data.hr_employee_id);
+                localStorage.setItem('Fullname', hr_data.hr_fullname_th);
+                localStorage.setItem('BaName', hr_data.hr_business_area);
+                localStorage.setItem('department', hr_data.hr_department);
+                redirectToIndex(hr_data);
             }
         });
     }
+
+    function checkHr() {
+        const data = JSON.parse(getSessionStorage('hr_data'));
+        console.log('hr_data', data);
+
+        if (data) {
+            window.location.href = "./dashboard.php";
+        }
+    }
+    checkHr();
+
 });
+
+function handleButtonClick() {
+    const employeeId = getSessionStorage('hr_employee_id'); // Check session storage for employee ID
+    console.log('employeeId in ss:', employeeId);
+
+
+    if (employeeId) {
+        // If employee ID exists in session storage, fetch user info directly
+        const hr_data = JSON.parse(getSessionStorage('hr_data'));
+        console.log('fetch from hr_data:', hr_data);
+        code = null;
+        redirectToIndex(hr_data);
+    } else {
+        // Redirect to the login page using the first proxy
+        const url = `${location.protocol}//${location.host}`;
+        const redirectUrl = url + "/pmac/serviceslogin/proxy1.php";
+        const queryParams = new URLSearchParams({
+            redirect_uri: url + "/pmac/views/index.php",
+            response_type: "code",
+            scope: "openid profile",
+            client_id: "pea-queuex-softkey",
+        });
+        window.location.href = `${redirectUrl}?${queryParams.toString()}`;
+    }
+}
+
+// Add an event listener to your button
+document.getElementById('loginButton').addEventListener('click', handleButtonClick);
+
+// $("#myform1").on("submit", function(e) {
+// e.preventDefault(); // ปิดการใช้งาน submit ปกติ เพื่อใช้งานผ่าน ajax
+// $("#loading").show();
+// // เตรียมข้อมูล form สำหรับส่งด้วย FormData Object
+
+// if ((!username) || (!password)) {
+// alert("กรอกข้อมูลไม่ครบ !!")
+// } else {
+// $.ajax({
+// type: "post",
+// url: "https://eitne2.com/nongha/api/login/",
+// contentType: "application/json",
+// dataType: "json",
+// data: JSON.stringify({
+// username: $("#username").val(),
+// password: $("#password").val(),
+// }),
+// success: function(response) {
+// if (response.statusCode == 200) {
+// localStorage.setItem('Peacode', response.user.Peacode);
+// localStorage.setItem('Username', response.user.Username);
+// localStorage.setItem('FirstName', response.user.FirstName);
+// localStorage.setItem('LastName', response.user.LastName);
+// localStorage.setItem('BaName', response.user.BaName);
+// window.location = "../views/dashboard.php";
+// } else {
+// alert("เข้าระบบมีปัญหาไม่สามารถเข้าระบบได้")
+// }
+// },
+// error: function(response) {
+// console.log(response);
+// }
+// });
+// }
+// });
 </script>
--- views/topbarlogout.php
+++ views/topbarlogout.php
@@ -1,11 +1,16 @@
 <nav class="navbar navbar-light bg-white topbar mb-4 static-top shadow">
     <a class="navbar-brand">Power Meter And Current Transformer Tester</a>
+    <span id="department" class="pull-right"></span>
     <form class="form-inline">
         <button class="btn btn-outline-danger my-2 my-sm-0" id="logout">Logout</button>
     </form>
 </nav>
 
 <script>
+    const Fullname = localStorage.getItem('Fullname');
+    $('#Fullname').text(Fullname);
+    const department = localStorage.getItem('department');
+    $('#department').text(Fullname+"|"+department);
 $("#logout").click(function(event) {
     event.preventDefault();
     alert(this.id);

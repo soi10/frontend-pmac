001ca0a3fc75f02eccab057f1fbddb201f58279b
Yongyuth Chaipa <nas.coke@gmail.com>
2024-11-07 08:50:36 +0700

    add logout with keycloak

--- views/topbarlogout.php
+++ views/topbarlogout.php
@@ -7,16 +7,24 @@
 </nav>
 
 <script>
-    const Fullname = localStorage.getItem('Fullname');
-    $('#Fullname').text(Fullname);
-    const department = localStorage.getItem('department');
-    $('#department').text(Fullname+"|"+department);
+const Fullname = localStorage.getItem('Fullname');
+$('#Fullname').text(Fullname);
+const department = localStorage.getItem('department');
+$('#department').text(Fullname + "|" + department);
+
+function removeSessionStorage(key) {
+    sessionStorage.removeItem(key);
+}
 $("#logout").click(function(event) {
     event.preventDefault();
     alert(this.id);
-
+    // Set the cookie with a past expiration date to delete it
+    removeSessionStorage('hr_employee_id');
+    removeSessionStorage('hr_data');
     localStorage.clear();
-
-    document.location.href = './dashboardguest.php'; //one level up
+    const url = `${location.protocol}//${location.host}/pmac/views/dashboardguest.php`;
+    window.location.href =
+        `https://sso2.pea.co.th/realms/pea-users/protocol/openid-connect/logout?redirect_uri=${url}`;
+    //document.location.href = './dashboardguest.php'; //one level up
 });
 </script>
